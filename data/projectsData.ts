3d8d03197effa4f83f886933d5106883b015d436
ekfkawl <ekfkawl>
2026-03-08 04:47:08 +0900

    update: projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '카카오스캔',
+    description: `번호 저장없이 상대방의 카카오톡 기본 프로필을 확인할 수 있는 서비스입니다.`,
+    imgSrc: '/static/images/kakaoscan-thumbnail.jpg',
+    href: '/blog/deep-dive-kakaoscan',
   },
 ]
 
